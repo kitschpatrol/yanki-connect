b1ce3d89ba8bd6e575aa098e3be24ea09e8d2c0b
Eric Mika <eric@ericmika.com>
2024-06-03 18:29:21 -0400

    Fix binding issue in proxied fetch.

--- src/client.ts
+++ src/client.ts
@@ -97,7 +97,7 @@ export type YankiConnectOptions = {
 
 export const defaultYankiConnectOptions: YankiConnectOptions = {
 	autoLaunch: false,
-	customFetch: fetch,
+	customFetch: fetch.bind(globalThis),
 	host: 'http://127.0.0.1',
 	key: undefined,
 	port: 8765,
